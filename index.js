604816cbb8113d145b86f5c87f94a725be2108c0
Ichthyological Dev <ichthyologicaldev@gmail.com>
2024-08-25 18:22:50 -0700

    add character count

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ document.getElementsByTagName('textarea')[0].addEventListener('input' , handleIn
 let codedMap = {};
 
 function handleInput(e) {
-    const text = [...e.target.value.toLowerCase()];
+    const text = [...e.target.value.toUpperCase()];
     // create hashmap of characters
     const charMap = text.reduce((map, item, index) => {
         if(!map[item]) {
@@ -14,23 +14,26 @@ function handleInput(e) {
     codedMap = charMap;
     quote.innerHTML = '';
 
-    for(const character of text) {
-        const characterDiv = document.createElement('div');
-        const characterInput = document.createElement('input')
-        const characterSpan = document.createElement('span')
-        characterDiv.appendChild(characterInput);
-        characterDiv.appendChild(characterSpan);
-        characterSpan.innerText = character;
-        characterInput.addEventListener('input', handleGuess)
-        if(['(', ')', "'", ',', '.', ' '].includes(character)) {
-            characterInput.classList.add('space');
+    let characterIndex = 0;
+    while(characterIndex < text.length) {
+        const wordDiv = document.createElement('div');
+        wordDiv.classList.add('word')
+        while(text[characterIndex] !== ' ' && characterIndex < text.length) {
+            const characterDiv = makeCharacterDiv(text[characterIndex]);
+            if(/[A-Z]/.test(text[characterIndex])) {
+                characterDiv.querySelector('input').addEventListener('input', handleGuess);
+            }
+            wordDiv.appendChild(characterDiv);
+            characterIndex++;
         }
-        quote.appendChild(characterDiv);
+        quote.appendChild(wordDiv);
+        characterIndex++;
     }
+    createCount();
 }
 
 function handleGuess(e) {
-    const guess = e.target.value;
+    const guess = e.target.value.toUpperCase();
     const original = e.target.parentNode.querySelector('span').innerText;
     fillOutGuess(original, guess);
 }
@@ -40,5 +43,32 @@ function fillOutGuess(original, guess) {
     const indices = codedMap[original];
     for(const index of indices) {
         inputs[index].value = guess;
+    }
+}
+
+function makeCharacterDiv(character) {
+    const characterDiv = document.createElement('div');
+    characterDiv.classList.add('character')
+    const characterInput = document.createElement('input')
+    const characterSpan = document.createElement('span')
+    characterDiv.appendChild(characterInput);
+    characterDiv.appendChild(characterSpan);
+    characterSpan.innerText = character;
+    if(!/[A-Z]/.test(character)) {
+        characterInput.classList.add('non-alphabetic');
+    }
+    return characterDiv;
+}
+
+function createCount() {
+    count.innerHTML = '';
+    for(let charCode = 65; charCode <= 90; charCode++) {
+        const letter = String.fromCharCode(charCode);
+        const div = document.createElement('div');
+        div.classList.add('character');
+        const span = document.createElement('span');
+        span.innerText = `${letter}:${codedMap[letter] ? codedMap[letter].length : 0}`
+        div.appendChild(span)
+        count.appendChild(div)
     }
 }
